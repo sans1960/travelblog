76eeaf016b5ddefd616792d31c87d911c28287df
sans1960 <asanscliment@gmail.com>
2022-10-05 14:53:19 +0200

    owl-carousel

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -2,6 +2,11 @@
 @section('title')
     {{ $destination->name}}
 @endsection
+@section('estilo')
+<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    
+@endsection
 @section('content')
 
     <div class="container-fluid  d-flex justify-content-center align-items-center"  style="background-image: url({{ asset('storage/destination/'.$destination->image)}});height:300px; background-size:cover;background-position:center center;" >
@@ -18,9 +23,21 @@
     </div>
 <div class="container mt-5">
     <div class="row">
-        <div class="col-md-8 mx-auto">
-            
+        <div class="col-md-6 mx-auto owl-carousel owl-theme">
+            @foreach ($destination->imagedestination as $item)
+                <div class="card">
+                    <img src="{{ asset('storage/imagedestination/'.$item->image)}}" class="card-img-top" alt=".{{ $item->caption}}">
+                   <div class="card-body">
+                     <h5 class="card-title">{{ $item->title}}</h5>
+   
+                     </div>
+                </div>
+                
+            @endforeach
+
         </div>
+      
+        
     </div>
 </div>
     <div class="container mt-5
@@ -42,5 +59,28 @@
     </div>
 @endsection
 @section('js')
-
+<script src="{{ asset('js/jquery.js') }}"></script>
+<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+<script>
+     $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
+</script>
 @endsection
